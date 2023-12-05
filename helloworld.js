dd4ab78a9de52a36d48df8ea0163303364c4986a
Vladimir Gerov <vladdy97@gmail.com>
2023-12-05 14:16:57 +0100

    🧼 Clean Working Env

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+//ASI: Automatic Semicolon Insertion (Only in JavaSkript)
+//Define a variable for the greeting program
+let greeting = "Hello";
+
+//Define a variable for thee target of greeting
+let target = "World";
+
+//COmbine the Variables and Output the Result
+console.log(greeting + " " + target + "!");
